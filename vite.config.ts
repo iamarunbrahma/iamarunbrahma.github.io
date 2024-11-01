3377bf847373e0283e29b4dab3c396dd397f61ac
Arun Brahma <mithubrahma94@gmail.com>
2024-11-01 23:44:54 +0530

    // Update dependencies and remove obsolete public files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import createSitemapPlugin from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createSitemapPlugin({
+      hostname: 'https://arunbrahma.com',
+      dynamicRoutes: ['/'],
+      robots: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
+      ],
+      outDir: 'dist',
+      lastmod: new Date(),
+      changefreq: 'weekly',
+      priority: 1.0
+    }),
+  ],
   base: '/',
   build: {
     outDir: 'dist',
